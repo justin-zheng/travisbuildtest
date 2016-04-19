541f07297e9260622da24faef0a39775f63443ea
Justin Zheng <justinz@unity3d.com>
2016-04-19 15:57:35 -0700

    use full path

--- build.sh
+++ build.sh
@@ -16,11 +16,11 @@ sudo installer -dumplog -package Unity.pkg -target /
 
 
 echo "activate license"
-Unity.app/Contents/MacOS/Unity -quit -batchmode -serial I3-GKE5-PKF4-2F84-2CMS-PTD5 -username justinz@unity3d.com -password '1Newpassword' -logfile
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -serial I3-GKE5-PKF4-2F84-2CMS-PTD5 -username justinz@unity3d.com -password '1Newpassword' -logfile
 
 echo "return license"
 
-Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense -logfile
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -quit -batchmode -returnlicense -logfile
 
 echo === Done ===
 
